9b862ec223d182f847b193af755fc633e9af9edc
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2024-01-17 09:49:58 -0300

    include viewModel and liveData on Compose

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -114,7 +114,7 @@ class PlantDetailFragment : Fragment() {
             composeView.setContent {
                 // You're in Compose world!
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription(plantDetailViewModel = plantDetailViewModel)
                 }
             }
         }
